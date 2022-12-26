e8502066a97e53d997c6410f924c7a3c54395c32
ORIENTSOFTWARE\truong.nguyenvan <truongqn1703@gmail.com>
2022-12-26 09:29:45 +0700

    upload images

--- src/Components/Body/Client/SignUp.js
+++ src/Components/Body/Client/SignUp.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Swal from "sweetalert2";
+import axios from "axios";
 
 const SignUp = () =>{
     const getGenderValue = () => {
@@ -45,40 +46,47 @@ const SignUp = () =>{
         }
     }
 
-    const register = () => {
+    const register = async () => {
 
-        const data = { 
-            File : null,
-            Email : document.getElementById('email').value.trim(), 
-            Password : document.getElementById('password').value.trim(), 
-            FirstName : document.getElementById('first-name').value.trim(), 
-            LastName : document.getElementById('last-name').value.trim(), 
-            Gender : getGenderValue(), 
-            CardId : document.getElementById('card-id').value.trim(), 
-            PhoneNumber : document.getElementById('phone-number').value.trim(),
-            Address : null
-        };
-        console.log(data)
-        fetch('https://localhost:7109/api/Account/auth/sign-up', {
-          method: 'POST', // or 'PUT'
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-        })
-          .then((response) => response.json())
-          .then((results) => {
-            console.log('Success:', results);
-            if(results['code'] != 200)
-                showAlertError(results['message']);
-            if(results['code'] == 200) {
-                document.cookie= "Email=" + results['data'] + "end-email";
-                window.location.href = "http://localhost:3000/auth/confirm-account";
-            }
-          })
-          .catch((error) => {
-            console.error('Error:', error);
-          });
+        const formData = new FormData();
+        formData.append("File", document.getElementById('avatar').files[0]);
+        formData.append("Email" ,document.getElementById('email').value.trim());
+        formData.append("Password" , document.getElementById('password').value.trim() );
+        formData.append("FirstName" , document.getElementById('first-name').value.trim());
+        formData.append("LastName" , document.getElementById('last-name').value.trim());
+        formData.append("Gender" , getGenderValue());
+        formData.append("CardId" , document.getElementById('card-id').value.trim());
+        formData.append("PhoneNumber" , document.getElementById('phone-number').value.trim());
+        formData.append("Address", null);
+        console.log([...formData])
+
+        try {
+            const res = await axios.post("https://localhost:7109/api/Account/auth/sign-up", formData);
+
+            console.log(res)
+        } catch (e) { 
+            console.log(e)
+        }
+        // fetch('https://localhost:7109/api/Account/auth/sign-up', {
+        //   method: 'POST', // or 'PUT'
+        //   headers: {
+        //     'Content-Type': 'multipart/form-data',
+        //   },
+        //   body: formData,
+        // })
+        //   .then((response) => response.json())
+        //   .then((results) => {
+        //     console.log('Success:', results);
+        //     if(results['code'] != 200)
+        //         showAlertError(results['message']);
+        //     if(results['code'] == 200) {
+        //         document.cookie= "Email=" + results['data'] + "end-email";
+        //         window.location.href = "http://localhost:3000/auth/confirm-account";
+        //     }
+        //   })
+        //   .catch((error) => {
+        //     console.error('Error:', error);
+        //   });
         }
 
     return <>
@@ -99,7 +107,7 @@ const SignUp = () =>{
             <div style={{marginBottom: "10px"}}>
                 <label>
                     Giới tính: <br/>
-                    <input id="gender-1" checked type="radio" name="gender" style={{marginLeft: "70px"}} value="Nam"/>Nam
+                    <input id="gender-1" defaultChecked type="radio" name="gender" style={{marginLeft: "70px"}} value="Nam"/>Nam
                     <input id="gender-2" type="radio" name="gender" style={{marginLeft: "70px"}} value="Nữ" /> Nữ
                     <input id="gender-3" type="radio" name="gender" style={{marginLeft: "70px"}} value="Khác" /> Khác
                 </label>
